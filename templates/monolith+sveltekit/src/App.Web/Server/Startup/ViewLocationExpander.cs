691c02eca75cd20c7393c7d33c01c28cc911b368
Kevin Dietz <kdietz65@gmail.com>
2024-05-13 16:27:59 -0500

    Add .editorconfig

--- templates/monolith+sveltekit/src/App.Web/Server/Startup/ViewLocationExpander.cs
+++ templates/monolith+sveltekit/src/App.Web/Server/Startup/ViewLocationExpander.cs
@@ -1,25 +1,24 @@
-namespace App.Web.Server
+namespace App.Web.Server;
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+public class ViewLocationExpander : IViewLocationExpander
 {
-  using System.Collections.Generic;
-  using Microsoft.AspNetCore.Mvc.Razor;
-  
-  public class ViewLocationExpander : IViewLocationExpander
+  // /////////////////////////////////////////////////////////////////////////////////////////////
+  // Public Methods
+  // /////////////////////////////////////////////////////////////////////////////////////////////
+
+  public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
   {
-    // /////////////////////////////////////////////////////////////////////////////////////////////
-    // Public Methods
-    // /////////////////////////////////////////////////////////////////////////////////////////////
+    // We need this because the views have been moved from their default locations. This
+    // tells MVC where to look to find them.
 
-    public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
-    {
-      // We need this because the views have been moved from their default locations. This
-      // tells MVC where to look to find them.
+    yield return "/Server/Areas/{1}/Views/{0}.cshtml";
+    yield return "/Server/Areas/Shared/Views/{0}.cshtml";
+  }
 
-      yield return "/Server/Areas/{1}/Views/{0}.cshtml";
-      yield return "/Server/Areas/Shared/Views/{0}.cshtml";
-    }
-
-    public void PopulateValues(ViewLocationExpanderContext context)
-    {
-    }
+  public void PopulateValues(ViewLocationExpanderContext context)
+  {
   }
 }
